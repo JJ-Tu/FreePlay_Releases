34ad7414e27ba7ceb13d9fedca2b42b929a6fe9d
JJ-Tu <44393168+JJ-Tu@users.noreply.github.com>
2023-08-01 19:53:11 -0700

    Add files via upload

---
+++ Website Distributions Automation.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Email notifications
+
+# In[46]:
+
+
+import smtplib
+
+# Set up Gmail credentials
+gmail_username = "obv.thrw.awy.343@gmail.com"
+gmail_password = "bdxlzkgxxjckaxbi"
+
+
+# In[47]:
+
+
+def send_email(subject, body, recipient):
+    try:
+        # Connect to Gmail SMTP server
+        smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp_server.starttls()
+        
+        # Log in to your Gmail account
+        smtp_server.login(gmail_username, gmail_password)
+
+        # Compose the email message
+        message = f"Subject: {subject}\n\n{body}"
+
+        # Send the email
+        smtp_server.sendmail(gmail_username, recipient, message)
+
+        print("Email sent successfully!")
+
+    except Exception as e:
+        print("Error sending email:", e)
+
+    finally:
+        # Close the SMTP connection
+        smtp_server.quit()
+
+
+# In[48]:
+
+
+def send_notification():
+    subject = "New Tickets Released!"
+    body = "New tickets are now available on the website."
+    recipient = "jonathantu2000@gmail.com"  # Replace with the recipient's email address
+    send_email(subject, body, recipient)
+
+
+# # Web scraping
+
+# In[49]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the webpage you want to scrape
+url = "https://www.centertheatregroup.org/tickets/freeplay/"
+
+
+# Function to fetch the webpage's HTML content and extract the relevant section
+def get_relevant_section(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        h3_tag = soup.find("h3", string="Ahmanson Theatre")
+        div_tag = soup.find("div", {"class": "unit-aside unit-aside-panel", "id": "health-and-safety-policy"})
+        
+        # Extract the content between the h3 and div tags
+        relevant_section = ""
+        while h3_tag and h3_tag.next_sibling != div_tag:
+            relevant_section += str(h3_tag.next_sibling)
+            h3_tag = h3_tag.next_sibling
+        
+        return relevant_section
+    else:
+        return None
+
+
+# In[50]:
+
+
+# Main function to check for changes and trigger notifications
+def check_for_changes():
+    # Fetch the current section
+    current_section = get_relevant_section(url)
+    
+    # Read the previous section from a file or database
+    with open("previous_section.txt", "r") as file:
+        previous_section = file.read()
+    
+    # Compare the current and previous sections
+    if current_section != previous_section:
+        # Update the previous section with the current one
+        with open("previous_section.txt", "w") as file:
+            file.write(current_section)
+        
+        # Trigger the notification
+        send_notification()
+
+
+# In[51]:
+
+
+# Call the main function to check for changes
+check_for_changes()
+
+
+# In[ ]:
+
+
+
+
